223ed5b72a7cd9377c230eac844afeaf8ed39f3b
Daria Sova <todariasova@gmail.com>
2018-04-15 16:41:14 -0700

    Return Quote
    Change-Id: Iae18ee293c47e6421ca5e90fc1f2153722b83ee6

--- server.go
+++ server.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang/glog"
 )
 
 func usage() {
@@ -16,6 +21,43 @@ func usage() {
 
 func echoString(c *gin.Context) {
 	c.String(http.StatusOK, "Quote server is UP!")
+}
+
+type Quote struct {
+	Price float64
+	Stock string
+	// UserId    string
+	Timestamp int64
+	CryptoKey string
+}
+
+type Request struct {
+	Stock string
+}
+
+func getParams(c *gin.Context) Request {
+	request := Request{}
+	body, err := ioutil.ReadAll(c.Request.Body)
+
+	if err != nil {
+		glog.Error("Error processing request: %s", err)
+	}
+
+	err = json.Unmarshal(body, &request)
+	if err != nil {
+		glog.Error("Error parsing JSON: %s", err)
+	}
+
+	return request
+}
+
+func handleQuoteReq(c *gin.Context) {
+	req := getParams(c)
+	quote := getQuote(req.Stock)
+	//randomly wait
+
+	c.BindJSON(&quote)
+	c.IndentedJSON(http.StatusOK, quote)
 }
 
 func main() {
@@ -36,6 +78,20 @@ func main() {
 
 }
 
-// func return Quote() {
+func getQuote(stock string) Quote {
+	r := rand.New(rand.NewSource(getCurrentTs()))
+	price := r.Float64()
+	price = float64(int(price*100)) / 100
+	return Quote{
+		Price: price,
+		Stock: stock,
+		// UserId:    userid,
+		Timestamp: getCurrentTs(),
+		CryptoKey: "PXdxruf7H5p9Br19Si5hq",
+	}
 
-// }
+}
+
+func getCurrentTs() int64 {
+	return time.Now().UnixNano() / 1000000
+}
